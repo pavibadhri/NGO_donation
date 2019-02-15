ce39f17deee42b6fb82aa8b64833eb6c1fa43010
Shuo Fan <fanshuo2015@gmail.com>
2019-02-15 10:39:25 -0500

    Login layout

--- NGO_donation/Controllers/HomeController.cs
+++ NGO_donation/Controllers/HomeController.cs
@@ -50,28 +50,5 @@ namespace NGO_donation.Controllers
             }
             return View("Index");
         }
-        //[HttpPost]
-        //public ActionResult Index(tbl_user model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        using (DonationEntities db = new DonationEntities())
-        //        {
-        //            var v = db.tbl_user.Where(a => a.email.Equals(model.email) && a.password.Equals(model.password)).FirstOrDefault();
-        //            if (v != null)
-        //            {
-        //                if (v.roleid == 1)//If its an admin User,provide access to User Management & Doantion Management
-        //                    return RedirectToAction("Index", "UserManage");
-        //                else if (v.roleid == 2)//If its a regular User,gets redirected to User View Controller
-        //                    return RedirectToAction("Index", "Reguser");
-        //            }
-        //            else
-        //            {
-        //                ViewBag.NotValidUser = "Please check your login credentials!";
-        //            }
-        //        }
-        //    }
-        //    return View("Index");
-        //}
     }
 }
